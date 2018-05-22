c69a2c13cb3303c3b7001238053cc9df43f72e7d
Pamela Frieze <pfrieze@gmail.com>
2018-05-22 16:50:48 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,7 +29,7 @@ class Scraper
       end
       student_index_array
     end
-
+binding.pry
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url)) #uses open uri module
@@ -51,7 +51,7 @@ class Scraper
     end
     scraped_student[:profile_quote] = doc.css(".profile-quote").text
     scraped_student[:bio] = doc.css(".description-holder").css("p").text
-    
+
     scraped_student
   end
 end
